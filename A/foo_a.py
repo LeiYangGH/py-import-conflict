506311ba9b1888dd924a44ba36ecd8446281592a
LeiYangGH <675686066@qq.com>
2020-10-12 23:21:06 +0800

    utils_module

--- A/foo_a.py
+++ A/foo_a.py
@@ -1,7 +1,9 @@
-from utils import echo_a
+from utils_module.utils import echo_a #when run B: ImportError: cannot import name 'echo_a'
+# from .utils_module.utils import echo_a #if use relative import, when run A: ModuleNotFoundError: No module named '__main__.utils_module'; '__main__' is not a package
 
 def foo_a():
     echo_a()
+
 
 if __name__ == '__main__':
     echo_a()
